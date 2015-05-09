e5b0e533d25f5352293238a617388ef465410de7
Mylene A. King <mylene.king@gmail.com>
2015-05-09 14:26:03 +0800

    Create thumbnails with Carrierwave

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -4,7 +4,7 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -47,5 +47,9 @@ class PictureUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+
+  version :thumb do
+    process :resize_to_fill => [50, 50]
+  end
 
 end
